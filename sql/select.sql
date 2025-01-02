26f8f7ed436eff6311f10034b84d0312d3d2785f
xdsun777 <charmgo@qq.com>
2025-01-02 10:57:54 +0800

    add sql

---
+++ sql/select.sql
@@ -0,0 +1,5 @@
+-- SELECT "_rowid_",* FROM "main"."zhibo2" WHERE "时间" REGEXP '2024-12-1[6789]' AND "时间" REGEXP '2024-12-2[01234]';
+-- SELECT "_rowid_",* FROM "main"."zhibo2" WHERE "时间" REGEXP '2024-12-1[6789]' GROUP BY "Uid"
+-- UNION
+-- SELECT "_rowid_",* FROM "main"."zhibo2" WHERE "时间" REGEXP '2024-12-2[01234]' GROUP BY "Uid" ORDER BY "主播昵称";
+-- SELECT * FROM "main"."zhibo2"  GROUP BY "Uid";
